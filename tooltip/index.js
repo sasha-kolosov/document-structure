d455ef49869f3a4bf7bba8cdde98ce9a732e0549
sasha-kolosov <77949996+sasha-kolosov@users.noreply.github.com>
2022-10-20 23:18:55 +0300

    Update index.js

--- tooltip/index.js
+++ tooltip/index.js
@@ -3,16 +3,12 @@ const hasTooltip = document.getElementsByClassName('has-tooltip')
 
 for(let i = 0; i < hasTooltip.length; i++) {
     hasTooltip[i].addEventListener('click', () => {
-        tooltip.classList.add('tooltip_active')
+        tooltip.classList.toggle('tooltip_active')
         tooltip.innerHTML = hasTooltip[i].getAttribute('title')
 
         const position = hasTooltip[i].getBoundingClientRect()
 
         tooltip.style.left = `${position.x}px`
         tooltip.style.top = `${position.y + position.height}px`
-
-        setTimeout(() => {
-            tooltip.classList.remove('tooltip_active')
-        }, 2000)
     })
 }
